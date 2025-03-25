c424641cf0e35d2cd2b2bf52348ddcf0ce1b4050
pesalxyz <faisalneverlone@gmail.com>
2025-03-25 19:21:23 +0700

    tugas 3

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+const printFibonacci = (n) => {
+    let fib = [0, 1];
+  
+    for (let i = 2; i < n; i++) {
+      fib.push(fib[i - 1] + fib[i - 2]);
+    }
+  
+    console.log(fib);
+  };
+  
+  printFibonacci(10);
